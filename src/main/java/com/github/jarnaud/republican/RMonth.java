0a9310db9506743546fdb0652e1950db7a123223
Jean Arnaud <jarnaud26@gmail.com>
2020-12-03 08:22:53 +0100

    Javadoc

--- src/main/java/com/github/jarnaud/republican/RMonth.java
+++ src/main/java/com/github/jarnaud/republican/RMonth.java
@@ -1,28 +1,31 @@
 package com.github.jarnaud.republican;
 
+/**
+ * A Republican month.
+ */
 public enum RMonth {
 
-    // Automn
+    // Automn.
     Vendemiaire(1, "Vendemiaire"),
     Brumaire(2, "Brumaire"),
     Frimaire(3, "Frimaire"),
 
-    // Winter
+    // Winter.
     Nivose(4, "Nivose"),
     Pluviose(5, "Pluviose"),
     Ventose(6, "Ventose"),
 
-    //Spring
+    //Spring.
     Germinal(7, "Germinal"),
     Floreal(8, "Floreal"),
     Prairial(9, "Prairial"),
 
-    // Summer
+    // Summer.
     Messidor(10, "Messidor"),
     Thermidor(11, "Thermidor"),
     Fructidor(12, "Fructidor"),
 
-    // Special (complementary) days
+    // Special (complementary) days.
     Sanculottide(13, "Sanculottide");
 
     private final int month;
@@ -33,10 +36,20 @@ public enum RMonth {
         this.name = name;
     }
 
+    /**
+     * Return the month number (eg. 1 for Vendemiaire, 12 for Fructidor, 13 for Sanculottide...)
+     *
+     * @return the month number.
+     */
     public int getMonth() {
         return month;
     }
 
+    /**
+     * The month name (eg. Fructidor, Prairial...)
+     *
+     * @return the month name.
+     */
     public String getName() {
         return name;
     }
